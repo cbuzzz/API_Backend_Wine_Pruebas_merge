696e831001b7976dce6d75fde40fbb5c3b8b1b79
danielperezflorit <daniel.perez.florit@estudiantat.upc,edu>
2024-10-24 22:22:28 +0200

    Añadido Create y Delete vinos

--- src/modelos/types_d_wine.ts
+++ src/modelos/types_d_wine.ts
@@ -1,6 +1,28 @@
-import { model, ObjectId, Schema } from "mongoose";
+//import { model, ObjectId, Schema } from "mongoose";
+import { model, Schema } from "mongoose";
 
 export interface wineInterface{
+    owner: string,
+    name: string,
+    price: number,
+    color: string,
+    brand: string,
+    grapetype: string,
+    habilitado: boolean
+}
+
+export const wineSchema = new Schema<wineInterface>({
+    owner: String,
+    name: String,
+    price: Number,
+    color: String,
+    brand: String,
+    grapetype: String,
+    habilitado: Boolean
+})
+
+
+/*export interface wineInterface{
     owner: ObjectId,
     name: string,
     price: number,
@@ -18,6 +40,6 @@ export const wineSchema = new Schema<wineInterface>({
     brand: String,
     grapetype: String,
     habilitado: Boolean
-})
+})*/
 
 export const wineofDB = model<wineInterface>('wine',wineSchema)
